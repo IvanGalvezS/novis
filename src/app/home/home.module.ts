8e00088c47c3f84d4725d41bfae01373a02569c2
Ivan <ivan.galvez.s@gmail.com>
2020-11-25 23:24:54 -0300

    final

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class HomeModule { }
